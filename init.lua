aa861e26918b0708f80c7ce4b793cc6d0ee984f9
ronrob-lu <ronrob@emojized.com>
2026-07-06 12:43:05 -0400

    agi new aber die ki wird uns noch nicht übernehmen

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ minetest.register_entity(modname .. ":npc", {
     initial_properties = {
         hp_max = 20,
         physical = true,
-        collisionbox = {-0.3, -1.0, -0.3, 0.3, 0.8, 0.3},
+        collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
         visual = "mesh",
         mesh = "character.b3d",
         textures = {npc_textures[1]},
@@ -34,7 +34,7 @@ minetest.register_entity(modname .. ":npc", {
         end
         self._texture = tex
         self.object:set_properties({textures = {tex}})
-
+        
         -- Winking/digging animation for character.b3d is frames 189-198
         self.object:set_animation({x=189, y=198}, 30, 0)
         self.object:set_acceleration({x=0, y=-9.81, z=0})
@@ -50,7 +50,7 @@ minetest.register_entity(modname .. ":npc", {
             self.timer = 0
             local pos = self.object:get_pos()
             local node = minetest.get_node(pos)
-
+            
             if minetest.get_item_group(node.name, "lava") > 0 then
                 self.object:punch(self.object, 1.0, {
                     full_punch_interval = 1.0,
@@ -72,22 +72,21 @@ minetest.register_craftitem(modname .. ":spawner", {
     on_place = function(itemstack, placer, pointed_thing)
         if pointed_thing.type == "node" then
             local pos = pointed_thing.above
-            pos.y = pos.y + 0.5
             local obj = minetest.add_entity(pos, modname .. ":npc")
-
+            
             if obj and placer and placer:is_player() then
                 local placer_pos = placer:get_pos()
                 local dir = vector.subtract(placer_pos, pos)
                 local yaw = minetest.dir_to_yaw(dir)
                 obj:set_yaw(yaw)
             end
-
+            
             local player_name = placer and placer:get_player_name() or ""
             local is_creative = false
             if minetest.is_creative_enabled and minetest.is_creative_enabled(player_name) then
                 is_creative = true
             end
-
+            
             if not is_creative then
                 itemstack:take_item()
             end
